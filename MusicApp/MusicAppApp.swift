07db56dd39f1d04fb2f7daf45ddd186ea322e723
Vitaliy <vmolokov59@gmail.com>
2023-06-08 19:16:42 +0300

    Initial Commit

---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by Виталик Молоков on 08.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
